0c6ad1e383bddd8c20156d896a80a7993f285293
joekcowan <joekcowan@gmail.com>
2024-08-29 12:38:03 -0400

    update to file structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+  include 'includes/def_head_content.php';
+?>
+
+
+
+<body>
+  <header>
+    <!-- place navbar here -->
+  </header>
+  <main>test</main>
+
+  <?php
+  include 'includes/def_foot_content.php';
+  ?>
